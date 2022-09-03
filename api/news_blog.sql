1518b72ae460dabd0a0f0308aa45b5c9872ee450
koreicnurs <koreicnurs@gmail.com>
2022-09-03 12:51:19 +0600

    create DB and add sql fail

---
+++ api/news_blog.sql
@@ -0,0 +1,28 @@
+create table news
+(
+    id          int auto_increment
+        primary key,
+    title       varchar(255)                        not null,
+    description text                                not null,
+    image       text                                null,
+    datetime    timestamp default CURRENT_TIMESTAMP null
+);
+
+create table comments
+(
+    id      int          not null
+        primary key,
+    author  varchar(255) null,
+    message text         not null,
+    news_id int          not null,
+    constraint comments_news_null_fk
+        foreign key (news_id) references news (id)
+            on update cascade on delete cascade
+);
+
+INSERT INTO news (title, description, image)
+VALUES ('Comics', 'about marvel', null);
+
+INSERT INTO comments (author, message, news_id)
+VALUES ('iron-man', 'i will be back', 1);
+
