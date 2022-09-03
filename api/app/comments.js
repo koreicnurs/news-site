8677284069845f642d8dd2e37ef731d53464e6fb
koreicnurs <koreicnurs@gmail.com>
2022-09-03 14:07:34 +0600

    add post request for comments and news

--- api/app/comments.js
+++ api/app/comments.js
@@ -8,4 +8,26 @@ router.get('/', async (req, res) => {
     res.send(comments);
 });
 
+router.post('/',  async (req, res) => {
+    if (!req.body.message || !req.body.news_id) {
+        return res.status(400).send({error: 'Something are missing'});
+    }
+
+    const comment = {
+        author: req.body.author,
+        message: req.body.message,
+        news_id: req.body.news_id
+    };
+
+    const newComment = await mySqlDb.getConnection().query(
+        'INSERT INTO ?? (author, message, news_id) VALUES (?, ?, ?)',
+        ['comments', comment.author, comment.message, comment.news_id]
+    )
+
+    res.send({
+        ...comment,
+        id: newComment.insertId,
+    });
+});
+
 module.exports = router;
--- api/app/news.js
+++ api/app/news.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const mySqlDb = require('../mySqlDb');
 
+const multer = require('multer');
+const path = require('path');
+const config = require('../config');
+const {nanoid} = require('nanoid');
+
+const mySqlDb = require('../mySqlDb');
 const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({storage});
 
 router.get('/', async (req, res) => {
     const [news] = await mySqlDb.getConnection().query('SELECT * from news');
@@ -14,6 +30,31 @@ router.get('/:id', async (req, res) => {
         ['news', req.params.id]
     );
     res.send(one_news[0]);
+});
+
+router.post('/', upload.single('image'),  async (req, res) => {
+    if (!req.body.title || !req.body.description) {
+        return res.status(400).send({error: 'Something are missing'});
+    }
+
+    const oneNews = {
+        title: req.body.title,
+        description: req.body.description,
+    };
+
+    if (req.file) {
+        oneNews.image = req.file.filename;
+    }
+
+    const newOneNews = await mySqlDb.getConnection().query(
+        'INSERT INTO ?? (title, description, image) VALUES (?, ?, ?)',
+        ['news', oneNews.title, oneNews.description, oneNews.image]
+    )
+
+    res.send({
+        ...oneNews,
+        id: newOneNews.insertId,
+    });
 });
 
 
